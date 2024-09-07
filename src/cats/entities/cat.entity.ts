dc2fe93d8b672a1e329766d03d01e343b55362ca
Fabian_PC <fb-224651@hs-weingarten.de>
2024-09-07 16:07:59 +0200

    Rework the cat entity the imitate db entity

--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,1 +1,10 @@
-export class Cat {}
+export class Cat {
+  constructor(
+    public id: number,
+    public name: string,
+    public age: number,
+    public breed: string,
+    public createdAt: Date = new Date(),
+    public lastUpdatedAt: Date = new Date(),
+  ) {}
+}
